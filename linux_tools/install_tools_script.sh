e77e2f58ebf94b8b77cf1ae275fb3d97e17a0574
fengming1201 <2569671765@qq.com>
2023-03-02 10:52:36 +0800

    add new pack 7z

--- linux_tools/install_tools_script.sh
+++ linux_tools/install_tools_script.sh
@@ -53,12 +53,49 @@ EOF
 
 function buildin_install_z_pkg
 {
-	local z_pkg=${fengming_tools_dir}/z.tar.gz
-	local target_dir=/opt
+	local z_pkg=${tools_dir}/z.tar.gz
+	local target_path=/opt
+	local target_dir=z
 
-	if [ -d ${target_dir}/z ];then echo "WARNNING:${FUNCNAME},allready installed.";return 0;fi
-	tar -zxvf ${z_pkg} -C ${target_dir}
-	if [ ! -d ${target_dir}/z ];then echo "ERROR:${FUNCNAME},tar fail...";return 1;fi
+	if [ -d ${target_path}/${target_dir} ];then echo "WARNNING:${FUNCNAME},allready installed.";return 0;fi
+	tar -zxvf ${z_pkg} -C ${target_path}
+	if [ $? -ne 0 ];then echo "ERROR:${FUNCNAME},tar fail...";return 1;fi
+
+	return 0
+}
+
+function buildin_install_7zz_pkg
+{
+	local z7z_pkg=${tools_dir}/7z2201-linux-x64.tar.xz
+	local target_path=/opt
+	local target_dir=7z
+	local link_bin_dir=/usr/local/bin
+
+	which 7zz > /dev/null
+	if [ $? -eq 0 ];then echo "WARNNING:${FUNCNAME},allready installed.";return 0;fi
+
+	if [ -d ${target_path}/${target_dir} ];then echo "WARNNING:${FUNCNAME},allready installed.";return 0;fi
+
+	mkdir -p ${target_path}/${target_dir}
+	if [ $? -ne 0 ];then echo "ERROR:${FUNCNAME},mkdir ${target_path}/${target_dir} fail";return 1;fi
+	if [ ! -f ${z7z_pkg} ];then echo "ERROR:${FUNCNAME},file:${z7z_pkg} no exist!";return 2;fi
+	tar -xJf ${z7z_pkg} -C ${target_path}/${target_dir}
+	if [ $? -ne 0 ];then echo "ERROR:${FUNCNAME},tar fail...";return 2;fi
+
+	#build link
+	if [ -e ${target_path}/${target_dir}/7zz ]
+	then
+		ln -s ${target_path}/${target_dir}/7zz  ${link_bin_dir}/7zz
+	else
+		echo "ERROR:${FUNCNAME},file:${target_path}/${target_dir}/7zz no exist!"
+	fi
+	if [ -e ${target_path}/${target_dir}/7zzs ]
+	then
+		ln -s ${target_path}/${target_dir}/7zzs  ${link_bin_dir}/7zzs
+	else
+		echo "ERROR:${FUNCNAME},file:${target_path}/${target_dir}/7zzs no exist!"		
+	fi
+
 	return 0
 }
 
@@ -66,7 +103,7 @@ function exe_func
 {
 	if [ $# -ne 1 ];then echo "ERROR:${FUNCNAME},paramter wrong!!";return 1;fi
 	$1
-	ret=$?
+	local ret=$?
 	if [ ${ret} -ne 0 ]
 	then 
 		echo "ERROR:${1} fail,ret=${ret}"
@@ -85,6 +122,8 @@ function tool_scheduler_func
 	exe_func buildin_install_vim_config_pkg
 
 	exe_func buildin_install_z_pkg
+
+	exe_func buildin_install_7zz_pkg
 
 	return 0
 }
