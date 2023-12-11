8ddd431d2f8611e498d4b98c39931dceb77e4823
luoshengming <luoshengming@vimtag.it>
2023-12-11 19:03:15 +0800

    add git token

--- fm_cmd_script/fm-gittoken_for_fengming.d.sh
+++ fm_cmd_script/fm-gittoken_for_fengming.d.sh
@@ -13,24 +13,54 @@ if [ "$1" = "show" ];then
     exit 0
 fi
 
+token_encrypt_file=${fengming_dir}/documents/sub_doc_git/git_my_github_token.txt
+
 function func_git_mytoken_manage
 {   
-    local git_token_list=("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" "end")
-	
-	echo "token:"
-	echo "${git_token_list[*]}"
-	echo "===================================="
-    echo "format:"
-    echo "git remote set-url origin  https://<your_token>@github.com/<USERNAME>/<REPO>.git"
-    echo "<your_token>：包括<>在内的全部字符替换成你的token"
-    echo "<USERNAME>：包括<>在内的全部字符替换成你的username"
-    echo "<REPO>：包括<>在内的全部字符替换成你要访问的仓库名称"
-	echo "e.g.:"
-    echo "git remote set-url origin  https://<your token>/fengming1201/fengming.d.git"
-	echo "or"
-	echo "git push origin main"
-	echo "username:"
-	echo "password:<your token>"
+    local tool=openssl
+    which ${tool} > /dev/null
+    if [ $? -ne 0 ]
+    then
+        echo "${tool} not found!!"
+        echo "apt install openssl"
+        return 1
+    fi
+    if [ $# -lt 1 ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]
+    then
+        echo ""
+        echo "$scriptname   encrypt/decrypt"
+        echo "$scriptname   [ 1 or encrypt ] \'token stream\'"
+        echo "$scriptname   [ 2 or decrypt ]"
+        echo ""
+        return 2
+    fi
+    local password="none"
+
+    if [ $1 -eq 1 ] || [ "$1" = "encrypt" ]
+    then
+        if [ $# -ne 2 ];then echo "ERROR:input at least two parameters";return 3;fi
+        password=$(read -p "input [encrypt] pass:")
+        echo -n "ciphertext:"
+        if [ -w ${token_encrypt_file} ]
+        then
+            #echo "echo -n "$2" |${tool} enc -aes-256-cbc -base64 -pbkdf2 -iter 100000 -pass pass:****** | tr -d '\n' | tee ${token_encrypt_file}"
+            echo -n "$2" |${tool} enc -aes-256-cbc -base64 -pbkdf2 -iter 100000 -pass pass:${password} | tr -d '\n' | tee ${token_encrypt_file}
+        else
+            #echo "echo -n "$2" |${tool} enc -aes-256-cbc -base64 -pbkdf2 -iter 100000 -pass pass:****** | tr -d '\n' |sudo tee ${token_encrypt_file}"
+            echo -n "$2" |${tool} enc -aes-256-cbc -base64 -pbkdf2 -iter 100000 -pass pass:${password} | tr -d '\n' |sudo tee ${token_encrypt_file}
+        fi
+        echo ""
+    elif [ $1 -eq 2 ] || [ "$1" = "decrypt" ]
+    then
+        password=$(read -p "input [decrypt] pass:")
+        local ciphertext=$(cat ${token_encrypt_file})
+        if [ "x${ciphertext}" = "x" ];then echo "ERROR:ciphertext file is empty.";return 4;fi
+        echo -n "plaintext:"
+        echo "${ciphertext}" | openssl enc -d -aes-256-cbc -base64 -pbkdf2 -iter 100000 -pass pass:${password}
+        echo ""
+    else
+        echo "ERROR:unknow opt"
+    fi
 
     return 0
 }
