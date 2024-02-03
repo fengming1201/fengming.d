b304598fae529143f1890ce7c363558b50d1cc48
luoshengming <luoshengming@vimtag.it>
2024-02-04 02:21:17 +0800

    rename file and add new files

--- fm_cmd_script/fm-linux_displayer_screen_brightness_set.sh
+++ fm_cmd_script/fm-linux_displayer_screen_brightness_set.sh
@@ -2,14 +2,33 @@
 scriptfile=$0
 scriptname=$(basename ${scriptfile})
 fengming_dir=$FENGMING_DIR
+common_share_function=${fengming_dir}/fm_cmd_script/common_share_function.sh
+
+if [ -f ${common_share_function} ] && [ "include" = "enable" ]
+then
+    source ${common_share_function}
+fi
+#if unnecessary, please do not modify this function
+function func_location
+{
+    if [ -L ${scriptfile} ]
+    then
+        echo "location:${scriptfile}  --> $(readlink ${scriptfile})"
+    else
+        echo "location:${scriptfile}"
+    fi
+    return 0
+}
 if [ "$1" = "info" ];then
-    echo "location:${scriptfile}"
     echo "abstract:"
+    echo ""
+    func_location
     exit 0
 fi
 if [ "$1" = "show" ];then
-    echo "location:${scriptfile}"
     cat ${scriptfile}
+    echo ""
+    func_location
     exit 0
 fi
 if [ $(id -u) -ne 0 ];then
@@ -25,13 +44,13 @@ function func_linux_brightness_control
 	if [ $# -lt 1 ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]
 	then
 		echo "CN:"
-		echo "$scriptname  monitor  brightness(0-1)" 
+		echo "$scriptname  monitor_name  brightness_value(0-1)" 
 		echo "$scriptname  HDMI-2  0.5"
+        echo "$scriptname  HDMI-2  #will give current value"
 		${app} -q | grep -w connected
 		return 1
 	fi
 	local monitor=$1
-	local brightness=$2
 	local  monitor_name=""
 	#check 
 	local monitor_list=$(${app} -q | grep -w connected | awk '{print$1}')
@@ -43,18 +62,26 @@ function func_linux_brightness_control
 		fi
 	done
 	if [ "x${monitor_name}" = "x" ];then echo "monitor name not found!";return 3;fi
-
+    local brightness=
+    if [ $# -eq 2 ]
+    then
+	    brightness=$2
+    else
+        if [ -e /sys/class/backlight/${monitor_name}/brightness ]
+        then
+            echo "current brightness value:$(cat /sys/class/backlight/${monitor_name}/brightness)"
+        fi
+        read -p "input brightness value:" brightness
+    fi
 	echo "${app} --output ${monitor_name} --brightness ${brightness}"
 	${app} --output ${monitor_name} --brightness ${brightness}
 
 	return 0
 }
 
-
-func_linux_brightness_control $@
+func_linux_brightness_control "$@"
 ret=$?
-if [ ${ret} -ne 0 ]
-then 
+if [ ${ret} -ne 0 ];then 
     exit 1
 fi
 exit 0
