3eda6cc273ea26ecc8db62df49868220ee910458
luoshengming <luoshengming@vimtag.it>
2024-02-28 11:12:59 +0800

    rename file

---
+++ fm_cmd_script/fm-gitconfig_set.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+scriptfile=$0
+scriptname=$(basename ${scriptfile})
+fengming_dir=$FENGMING_DIR
+common_share_function=${fengming_dir}/fm_cmd_script/common_share_function.sh
+
+if [ -f ${common_share_function} ] && [ "include" = "enable" ]
+then
+    source ${common_share_function}
+fi
+#if unnecessary, please do not modify this function
+function func_location
+{
+    if [ -L ${scriptfile} ]
+    then
+        echo "location:${scriptfile}  --> $(readlink ${scriptfile})"
+    else
+        echo "location:${scriptfile}"
+    fi
+    return 0
+}
+if [ "$1" = "info" ] || [ "$1" = "-info" ] || [ "$1" = "--info" ];then
+    echo "abstract:"
+    echo ""
+    func_location
+    exit 0
+fi
+if [ "$1" = "show" ] || [ "$1" = "-show" ] || [ "$1" = "--show" ];then
+    cat ${scriptfile}
+    echo ""
+    func_location
+    exit 0
+fi
+if [ $(id -u) -ne 0 ];then
+    maybeSUDO=sudo
+fi
+#start here add your code,you need to implement the following function.
+function func_
+{
+    #[optional]
+    #local need_help=no
+    #if [ "$1" != "1" ] && [ "$1" != "2" ] && [ "$1" != "param1" ] && [ "$1" != "param2" ];then need_help=yes;fi
+    #if [ $# -lt 1 ] || [ "$1" = "-h" ] || [ "$1" = "--help" ] || [ ${need_help} = "yes" ]
+    
+    if [ $# -lt 1 ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]
+    then
+        echo "$scriptname  param_list"
+        return 1
+    fi
+
+    return 0
+}
+
+func_ "$@"
+ret=$?
+if [ ${ret} -ne 0 ];then 
+    exit 1
+fi
+exit 0
---
+++ fm_cmd_script/fm-gitconfig_show_all.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+scriptfile=$0
+scriptname=$(basename ${scriptfile})
+fengming_dir=$FENGMING_DIR
+common_share_function=${fengming_dir}/fm_cmd_script/common_share_function.sh
+
+if [ -f ${common_share_function} ] && [ "include" = "enable" ]
+then
+    source ${common_share_function}
+fi
+#if unnecessary, please do not modify this function
+function func_location
+{
+    if [ -L ${scriptfile} ]
+    then
+        echo "location:${scriptfile}  --> $(readlink ${scriptfile})"
+    else
+        echo "location:${scriptfile}"
+    fi
+    return 0
+}
+if [ "$1" = "info" ] || [ "$1" = "-info" ] || [ "$1" = "--info" ];then
+    echo "abstract:"
+    echo ""
+    func_location
+    exit 0
+fi
+if [ "$1" = "show" ] || [ "$1" = "-show" ] || [ "$1" = "--show" ];then
+    cat ${scriptfile}
+    echo ""
+    func_location
+    exit 0
+fi
+if [ $(id -u) -ne 0 ];then
+    maybeSUDO=sudo
+fi
+#start here add your code,you need to implement the following function.
+function func_show_git_config_info
+{
+    if [ "$1" = "-h" ] || [ "$1" = "--help" ]
+    then
+        echo "$scriptname  not-arg"
+        return 1
+    fi
+    echo ""
+    git config --list
+    echo ""
+    return 0
+}
+
+func_show_git_config_info "$@"
+ret=$?
+if [ ${ret} -ne 0 ];then 
+    exit 1
+fi
+exit 0
