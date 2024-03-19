87eca0f1acdce362b50a6f2bac723bc93ba198a8
luoshengming <luoshengming@vimtag.it>
2024-03-19 15:24:26 +0800

    modify

--- fm_cmd_script/fm-upload_files_to_fileserver_tar_my.sh
+++ fm_cmd_script/fm-upload_files_to_fileserver_tar_my.sh
@@ -42,16 +42,16 @@ function func_upload_files_to_fileserver_tar_my
 	if [ $# -lt 1 ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]
 	then
 		echo ""
-		echo "$scriptname  file1 file2 fileN ... [target_dir num]"
-		echo "$scriptname  file1 file2                        #upload to /root/http_share/data"
-		echo "$scriptname  file1 file2   1                    #upload to /root/http_share/data/Andriod_Apk"
-		echo "$scriptname  file1 file2   8                    #upload to /root/http_share/data/unclassified"
+		echo "$scriptname  file_list   [target_dir num]"
+		echo "$scriptname  file_list     "
+		echo "$scriptname  file_list   1 "
+		echo "$scriptname  file_list   8 "
 		echo ""
 		echo "target_dir list:"
 		local index=1
 		for str in ${target_dir_list[*]}
 		do
-			echo "                ${index} -- ${str}"
+			echo "                ${index} -- ${str}/"
 			index=$(expr $index + 1)
 		done
 		echo ""
@@ -60,26 +60,31 @@ function func_upload_files_to_fileserver_tar_my
     local num_args=$#  # 获取参数个数
     local last_arg="${!num_args}"  # 获取最后一个参数
 	local file_list
-    local target_dir=/root/http_share/data/
+    local target_dir=/root/http_share/data
 	#check file stat
 	for file in "$@"
 	do
 		if [ ! -f ${file} ]
 		then
-			if [[ ${last_arg} =~ ^[0-9]+$ ]]; then
-				echo ""
-			else
-				echo "WARNING:file:${file} not found,ignore it"
+			echo "WARNING:file:${file} not found,ignore it"
+			if [ "${1}" -eq "${1}" ] 2>/dev/null; then
+				echo "the first parameter should not be a number"
+				retrun 2	
 			fi
 		else
 			file_list+=("${file}")
 		fi
 	done
-	if [[ ${last_arg} =~ ^[0-9]+$ ]]; then
+	if [ "${last_arg}" -eq "${last_arg}" ] 2>/dev/null; then
 		if [ ${last_arg} -le ${target_dir_list_size} ];then
 			target_dir=/root/http_share/data/${target_dir_list[$(expr ${last_arg} - 1)]}
 		fi
+	else
+	    target_dir=${target_dir}/unclassified
+		echo "Note: the target directory is:${target_dir}"
+
 	fi
+
     local ip="139.9.186.120"
     local port="1201"
     local username="root"
