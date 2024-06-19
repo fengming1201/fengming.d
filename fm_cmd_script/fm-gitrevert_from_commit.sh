862350bb80aa8ade211d74bbac13cba5c0732ff9
luoshengming <luoshengming@vimtag.it>
2024-06-19 11:34:49 +0800

    add encrypt file for git token managment

---
+++ fm_cmd_script/fm-gitrevert_from_commit.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+scriptfile=$0
+scriptname=$(basename ${scriptfile})
+fengming_dir=$FENGMING_DIR
+common_share_function=${fengming_dir}/fm_cmd_script/common_share_function.sh
+
+if [ -f ${common_share_function} ] && [ "include" = "enable" ]
+then
+    source ${common_share_function}
+fi
+#if unnecessary, please do not modify this function
+function func_location
+{
+    if [ -L ${scriptfile} ]
+    then
+        echo "location:${scriptfile}  --> $(readlink ${scriptfile})"
+    else
+        echo "location:${scriptfile}"
+    fi
+    return 0
+}
+if [ "$1" = "info" ] || [ "$1" = "-info" ] || [ "$1" = "--info" ];then
+    echo "abstract:"
+    echo "在 Git 中撤销最近的一次提交，你可以使用 git reset 命令。有几种不同的方式来撤销提交。"
+    echo "有几种不同的方式来撤销提交，具体取决于你是否希望保留更改或者完全丢弃更改。"
+    echo ""
+    echo ""
+    func_location
+    exit 0
+fi
+if [ "$1" = "show" ] || [ "$1" = "-show" ] || [ "$1" = "--show" ];then
+    cat ${scriptfile}
+    echo ""
+    func_location
+    exit 0
+fi
+if [ $(id -u) -ne 0 ];then
+    maybeSUDO=sudo
+fi
+#start here add your code,you need to implement the following function.
+function func_gitrevert
+{
+    local opt=soft
+    if [ $# -lt 1 ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]
+    then
+        echo "$scriptname  [sorft|1] [hard|2] [msg|3]"
+        echo ""
+        echo "$scriptname  sorft or 1    --保留更改，撤销提交。"
+        echo "$scriptname  hard  or 2    --撤销更改和提交。"
+        echo "$scriptname  msg   or 3    --仅更改最后一次提交的信息。"
+        echo ""
+        return 1
+    fi
+    git status > /dev/null 2>&1
+	if [ $? -ne 0 ]
+	then
+        maybeSUDO=sudo
+    else
+        maybeSUDO=
+    fi
+    if [ "x${opt}" = "xsorft" ] || [ ${opt} -eq 1 ]
+    then
+        ${maybeSUDO} git reset --soft HEAD~1
+    elif [ "x${opt}" = "xhard" ] || [ ${opt} -eq 2 ]
+    then
+        ${maybeSUDO} git reset --hard HEAD~1
+    elif [ "x${opt}" = "xmixed" ] || [ ${opt} -eq 2 ]
+    then
+        ${maybeSUDO} git reset --mixed HEAD~1        
+    elif [ "x${opt}" = "xmsg" ] || [ ${opt} -eq 3 ]
+    then
+        ${maybeSUDO} git commit --amend
+    else
+        echo "unknown opt:${opt}"
+    fi
+
+    return 0
+}
+
+func_gitrevert "$@"
+ret=$?
+if [ ${ret} -ne 0 ];then 
+    exit 1
+fi
+exit 0
---
+++ fm_cmd_script/fm-gittoken_managment.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+scriptfile=$0
+scriptname=$(basename ${scriptfile})
+fengming_dir=$FENGMING_DIR
+if [ "$1" = "info" ] || [ "$1" = "-info" ]|| [ "$1" = "--info" ];then
+    echo "location:${scriptfile}"
+    echo "abstract:"
+    exit 0
+fi
+if [ "$1" = "show" ] || [ "$1" = "-show" ] || [ "$1" = "--show" ];then
+    echo "location:${scriptfile}"
+    cat ${scriptfile}
+    exit 0
+fi
+if [ $(id -u) -ne 0 ];then
+    maybeSUDO=sudo
+fi
+token_encrypt_file_path=${fengming_dir}/encrypt_files
+
+function func_git_mytoken_manage
+{   
+    local tool=openssl
+    which ${tool} > /dev/null
+    if [ $? -ne 0 ]
+    then
+        echo "${tool} not found!!"
+        echo "apt install openssl"
+        return 1
+    fi
+    if [ $# -lt 1 ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]
+    then
+        echo ""
+        echo "$scriptname    encrypt/decrypt type"
+        echo "$scriptname   [ 1 or encrypt ] type \'token stream\'"
+        echo "$scriptname   [ 2 or decrypt ] type"
+        echo "type: github | gitlab | gitee, dedault is github"
+        echo ""
+        return 2
+    fi
+    local type=github
+    local targetfile="none"
+    local password="none"
+
+    if [ $1 -eq 1 ] || [ "$1" = "encrypt" ]
+    then
+        if [ $# -ne 3 ]
+        then 
+            echo "ERROR:input at least two parameters"
+            echo "e.g:$scriptname   [ 1 or encrypt ] type \'token stream\'"
+            echo "type: github | gitlab | gitee, dedault is github"
+            return 3
+        fi
+        type=$2
+        targetfile=${token_encrypt_file_path}/${type}_token_encrypt_file
+        echo "type=$type"
+        echo "targetfile=$targetfile"
+        if [ ! -f ${targetfile} ];then echo "ERROR:can not found file!";return 4;fi
+
+        read -s -p "input [encrypt] pass:"  password
+        echo ""
+        if [ "x${password}" = "x" ];then echo -e "\nERROR:Password cannot be empty!";return 4;fi
+        local  ciphertext=$(echo -n "$3" |${tool} enc -aes-256-cbc -base64 -pbkdf2 -iter 100000 -pass pass:${password} | tr -d '\n')
+        echo "${type} ciphertext-->: ${ciphertext}"
+        local  confirm=yes
+        if [ -s ${targetfile} ]
+        then
+            read -p "sub_doc_git/git_my_github_token.txt already has ciphertext, do you want to overwrite it?[y/N]:" confirm
+            if [ "x${confirm}" = "x" ];then confirm=N;fi
+        fi
+        if [ "${confirm}" = "y" ] || [ "${confirm}" = "Y" ] || [ "${confirm}" = "yes" ] || [ "${confirm}" = "YES" ]
+        then
+            if [ -w ${targetfile} ]
+            then
+                echo -n "${ciphertext}" | tee ${targetfile} > /dev/null
+            else
+                echo -n "${ciphertext}" | sudo tee ${targetfile} > /dev/null
+            fi
+            cat ${targetfile}
+            echo ""
+            echo "Save Success!!"
+        else
+            echo "Discard Save!!"
+        fi
+        echo ""
+    elif [ $1 -eq 2 ] || [ "$1" = "decrypt" ]
+    then
+        if [ $# -eq 2 ];then type=$2;fi
+        targetfile=${token_encrypt_file_path}/${type}_token_encrypt_file
+        echo "type=$type"
+        echo "targetfile=$targetfile"
+        if [ ! -f ${targetfile} ];then echo "ERROR:can not found file!";return 4;fi
+
+        read -s -p "input [decrypt] pass:" password
+        if [ "x${password}" = "x" ];then echo -e "\nERROR:Password cannot be empty!";return 4;fi
+        local ciphertext=$(cat ${targetfile})
+        if [ "x${ciphertext}" = "x" ];then echo "ERROR:ciphertext file is empty.";return 4;fi
+        echo ""
+        echo -n "${type} plaintext-->: "
+        echo "${ciphertext}" | openssl enc -d -aes-256-cbc -base64 -pbkdf2 -iter 100000 -pass pass:${password}
+        echo ""
+    else
+        echo "ERROR:unknow opt"
+    fi
+
+    return 0
+}
+
+func_git_mytoken_manage $@
+ret=$?
+if [ ${ret} -ne 0 ];then 
+    exit 1
+fi
+exit 0
