07ca934e054282d15bac70dff3e265e6ba873598
luoshengming <luoshengming@vimtag.it>
2025-03-11 18:03:47 +0800

    add new tool for git

---
+++ fm_cmd_script/fm-gittig.sh
@@ -0,0 +1,162 @@
+#!/bin/bash
+
+#if unnecessary, please do not modify following code
+scriptfile=$0
+scriptname=$(basename ${scriptfile})
+fengming_dir=$FENGMING_DIR
+#if unnecessary, please do not modify following code
+extern_script_files=(${fengming_dir}/fm_cmd_script/common_share_function.sh)
+if [ -f ${extern_script_files[0]} ];then
+    source ${extern_script_files[0]}
+fi
+
+if [ "$1" = "info" ] || [ "$1" = "-info" ] || [ "$1" = "--info" ];then
+    echo ""
+    echo "info | -info | --info                                 #优先级1: 显示摘要"
+    echo "show | -show | --show                                 #优先级2: 打印本脚本文件"
+    func_debug_help
+    echo ""
+    echo "abstract:"
+    echo "tig (Text-mode Interactive Git)是一个基于终端的 Git 仓库浏览器，支持图形化显示分支和提交历史。"
+    echo ""
+    echo ""
+    echo ""
+    echo ""
+    echo ""
+    echo ""
+    echo ""
+    echo ""
+    echo ""
+    func_location
+    exit 0
+fi
+#if unnecessary, please do not modify following code
+if [ "$1" = "show" ] || [ "$1" = "-show" ] || [ "$1" = "--show" ];then
+    cat ${scriptfile}
+    echo ""
+    func_location
+    exit 0
+fi
+
+if [ $(id -u) -ne 0 ] && [ ${USER} != $(ls -ld . | awk '{print$3}') ];then
+    maybeSUDO=sudo
+fi
+#start here add your code,you need to implement the following function.
+
+##Parameter Counts      : 0
+# Parameter Requirements: none
+# Example:
+##
+function usage
+{
+    echo ""
+    echo "$scriptname  [opt]  files"
+    echo "opt:"
+    echo "-h or --help       # help"
+    echo "-d or --debug      # print variable status"
+    echo "-t or --test       # test mode, no modifications"
+    #echo "--realdo          # real execution"
+    echo "-m or --mode       # you define"
+    echo "--setx or --detail # open set -x mode"
+    echo ""
+}
+
+##Parameter Counts      : 0
+# Parameter Requirements: none
+# Example:
+##
+function func_
+{
+    #if [ $# -lt 1 ];then usage; return 1; fi
+    local debug=false
+    local test=false
+    local realdo=false
+    local mode=normal
+    local setx=false
+    local cmd_opt=() #命令自身累加选项，,如-F test.txt 加--file test.txt,-Q 加 --qr=true。
+    local remaining_args=()
+    while [[ $# -gt 0 ]]
+    do
+        case "$1" in
+            -h|--help) usage; return 0 ;;
+            -d|--debug) debug=true; shift ;; #不带参数,移动1
+            -t|--test) test=true; shift ;;
+            --realdo) realdo=true; shift ;;
+            --setx) setx=true; shift ;; #不带参数,移动1
+            --detail) setx=true; shift ;; #不带参数,移动1
+            -m|--mode)
+                if [[ -z "$2" ]]; then echo "ERROR: this opt requires one parameter" >&2; return 1; fi
+                mode="$2"; shift 2 ;; #带参数,移动2
+            -F|--file)
+                if [[ -z "$2" ]]; then echo "ERROR: this opt requires one parameter" >&2; return 1; fi
+                cmd_opt+=("--file $2"); shift 2 ;; #带参数,移动2
+            -Q|--qr) cmd_opt+=("--qr=true"); shift ;;
+            -*)
+                # 处理合并的选项,如-dh
+                for (( i=1; i<${#1}; i++ )); do
+                    case ${1:i:1} in
+                        h) usage; return 0 ;;
+                        d) debug=true ;;
+                        t) test=true ;;
+                        m) mode="$2"; shift;break ;; # 当 m 是合并选项的一部分时，它应该停止解析剩余的字符
+                        F) cmd_opt+=("--file $2"); shift;break ;; # 当 m 是合并选项的一部分时，它应该停止解析剩余的字符
+                        Q) cmd_opt+=("--qr=true") ;;
+                        *) echo "ERROR: invalid option: -${1:i:1}" >&2; return 1 ;;
+                    esac
+                done
+                shift ;;
+            *) remaining_args+=("$1"); shift ;; # 非选项参数全部放入数组中
+        esac
+    done
+    #==================== print debug =============================#
+    if [ ${debug} = true ];then
+        echo "DEBUG:maybeSUDO=${maybeSUDO}"
+        echo "DEBUG:debug=${debug}"
+        echo "DEBUG:test=${test}"
+        #echo "DEBUG:realdo=${realdo}"
+        echo "DEBUG:mode=${mode}"
+        echo "DEBUG:setx=${setx}"
+        echo "DEBUG:cmd_opt=${cmd_opt[@]} "#累加选项,如-F test.txt 加--file test.txt,-Q 加 --qr=true。
+        echo "DEBUG:remaining_args=${remaining_args[@]}"
+    fi
+    #=================== start your code ==============================#
+    #if [ ${#remaining_args[@]} -lt 1 ];then
+    #    echo "ERROR: platform list is empty!!";usage;return 2
+    #fi
+    #start your code
+    #for file in "${remaining_args[@]}"
+    #do
+        #here we process each parameter
+        #linux_cmd  ${cmd_opt[@]} args ....
+    #done
+    local tmp_file=$(mktemp)
+    git status > ${tmp_file}  2>&1
+	local ret=$?
+	if [ "x$(grep "not a git repository" ${tmp_file})" != x ] && [ ${ret} -ne 0 ]
+	then 
+		echo "fatal: this dir:$(pwd) not in git repository"
+		rm -f ${tmp_file}
+		return 1
+	fi
+	rm -f ${tmp_file}
+
+    local app=tig
+    which ${app} > /dev/null
+    if [ $? -ne 0 ];then
+        echo "ERROR: ${app} not install yet"
+        echo "please install it first!"
+        echo "apt install tig"
+        return 3
+    fi
+    
+    ${app}
+
+    return 0
+}
+#if unnecessary, please do not modify following code
+func_debug_function "$@"
+if [ $? -ne 0 ];then exit 0;fi
+
+func_ "$@"
+if [ $? -ne 0 ];then exit 1;fi
+exit 0
