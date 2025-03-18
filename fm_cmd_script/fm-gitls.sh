fdf0dd90adb96c3c5ed4b934dcbc75b992f0c888
luoshengming <2569671765@qq.com>
2025-03-18 21:44:39 +0800

    update

--- fm_cmd_script/fm-gitls.sh
+++ fm_cmd_script/fm-gitls.sh
@@ -24,12 +24,31 @@ function func_gitls
 	fi
 
 	git status > /dev/null 2>&1
+	ret=$?
+	if [ ${ret} -ne 0 ]
+	then
+		echo "{"
+		sudo git remote -v
+		echo "},"
+		echo "{"
+		sudo git branch
+		echo "},"
+	else
+		echo "{"
+		git remote -v
+		echo "},"
+		echo "{"
+		git branch
+		echo "},"
+	fi
+	echo "{"
 	if [ $? -ne 0 ]
 	then
 		sudo git status | grep -E 'new file:|modified:|deleted:|renamed:|copied:'
 	else
 		git status | grep -E 'new file:|modified:|deleted:|renamed:|copied:'
 	fi
+	echo "}"
 	return 0
 }
 
